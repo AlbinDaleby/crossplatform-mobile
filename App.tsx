786b9d145068ccaeac087e231c2a68bedd4ea2eb
Albin Daleby <albin.daleby@gmail.com>
2023-10-30 13:09:15 +0100

    day 1 monday, setup project, added react navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button } from '@rneui/base';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen(props) {
+  const { navigation } = props
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }} />
+        <Tab.Screen name="Details" component={DetailsScreen} /> 
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/UserList/UserList.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const UserList = () => {
+	return (
+		<View>
+			<Text>UserList</Text>
+		</View>
+	)
+}
+
+export default UserList
