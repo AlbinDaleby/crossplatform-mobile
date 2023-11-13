1de9c99105d03d1e496e46c75755f7d07015a5ff
Albin Daleby <albin.daleby@gmail.com>
2023-11-13 09:48:32 +0100

    added eslint

--- src/screens/UserList/UserList.tsx
+++ src/screens/UserList/UserList.tsx
@@ -1,26 +1,33 @@
-import { View, Text, FlatList } from 'react-native'
-import { useGetUsersQuery } from '../../store/api/usersApi'
-import { ListItem } from '@rneui/themed'
+import { ListItem } from "@rneui/themed";
+import { View, Text, FlatList } from "react-native";
+
+import { useGetUsersQuery } from "../../store/api/usersApi";
 
 const UserList = ({ navigation }) => {
-	const { data, isLoading } = useGetUsersQuery({})
-	return (
-		<View>
-			{isLoading ? <Text>Loading...</Text> : (
-				<FlatList data={data} renderItem={({ item }) => (
-					<ListItem
-						key={item.id}
-						onPress={() => { navigation.navigate('UserInfo', { user: item }) }}
-					>
-						<ListItem.Content>
-							<ListItem.Title>{`${item.firstName} ${item.lastName}`}</ListItem.Title>
-						</ListItem.Content>
-					</ListItem>
-				)}>
-				</FlatList>
-			)}
-		</View>
-	)
-}
+  const { data, isLoading } = useGetUsersQuery({});
+  return (
+    <View>
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <ListItem
+              key={item.id}
+              onPress={() => {
+                navigation.navigate("UserInfo", { user: item });
+              }}
+            >
+              <ListItem.Content>
+                <ListItem.Title>{`${item.firstName} ${item.lastName}`}</ListItem.Title>
+              </ListItem.Content>
+            </ListItem>
+          )}
+        />
+      )}
+    </View>
+  );
+};
 
-export default UserList
+export default UserList;
--- src/store/store.ts
+++ src/store/store.ts
@@ -3,13 +3,20 @@ import { setupListeners } from '@reduxjs/toolkit/query';
 import { usersApi } from './api/usersApi';
 import authSlice from './slices/authSlice';
 
+const middlewares = [usersApi.middleware];
+
+if (process.env.NODE_ENV === `development`) {
+	const { logger } = require(`redux-logger`);
+	middlewares.push(logger);
+}
+
 export const store = configureStore({
-  reducer: {
-    [usersApi.reducerPath]: usersApi.reducer,
-    auth: authSlice
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(usersApi.middleware),
+	reducer: {
+		[usersApi.reducerPath]: usersApi.reducer,
+		auth: authSlice,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(...middlewares),
 });
 
 setupListeners(store.dispatch);
